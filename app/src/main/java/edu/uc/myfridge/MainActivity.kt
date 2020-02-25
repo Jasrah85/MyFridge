2eb369c832488857852842758402d6ad01bc98ce
Jasrah85 <alainna.nichols85@gmail.com>
2020-02-25 15:45:57 -0500

    Initial commit

---
+++ app/src/main/java/edu/uc/myfridge/MainActivity.kt
@@ -0,0 +1,19 @@
+package edu.uc.myfridge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.uc.myfridge.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
